bb06f0645726fed2e8f6d6fde50d3ce7fc5fd311
JoeSuzuki <joesuzuki516@gmail.com>
2018-11-18 13:37:00 -0500

    Fix structure

--- util/sensors/IMU.java
+++ util/sensors/IMU.java
@@ -5,7 +5,7 @@ import com.qualcomm.hardware.bosch.BNO055IMU;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 
 public interface IMU {
-    public void init(BNO055IMU f, AxesOrder axesOrder, double heading);
-    public double getHeading();
-    public double getError(double d);
+    void init(BNO055IMU f, AxesOrder axesOrder, double heading);
+    double getHeading();
+    double getError(double d);
 }
